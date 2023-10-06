5727e5d509e3a91191c5a49f51ae000e4fe2dfcd
SebastianGuarna <SebastianGuarna@localhost>
2023-10-06 15:55:36 +1100

    File update: Begin drafting of 'test.m' content, began work on the .ply model of the GoFa 5

--- test.m
+++ test.m
@@ -1,2 +1,9 @@
-%% FILE 1
+%% Sample Draft File
 % test
+% workspace = PlaceObject('Workspace.ply', [0,0,0]);
+% workspace = PlaceObject('Workspace_Simple.ply', [0,0,0]);
+% 
+% hold on;
+% 
+% % Create an ABB GoFa 5 model
+% GoFa = ABBGoFa();
